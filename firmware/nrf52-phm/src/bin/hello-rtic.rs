01eb0cbd0674712effb766cc7059c2211ea09f7a
Henrik Alsér <henrik.alser@me.com>
2022-02-07 00:58:39 +0100

    Port firmware to RTIC 1.0 (#2)
    * Port to RTIC 1.0
    
    * Port firmware to RTIC 1.0
    
    * Fix merge error
    
    * Add author

--- firmware/nrf52-phm/src/bin/hello-rtic.rs
+++ firmware/nrf52-phm/src/bin/hello-rtic.rs
@@ -1,48 +1,54 @@
 #![no_main]
 #![no_std]
 
-use cortex_m::singleton;
-use embedded_hal::blocking::i2c::Write;
-use nrf52840_hal::{
-    clocks::{ExternalOscillator, Internal, LfOscStopped},
-    gpio::p1::Parts as P1Parts,
-    pac::TWIM0,
-    twim::{Frequency, Pins as TwimPins, Twim},
-    usbd::{UsbPeripheral, Usbd},
-    Clocks,
-};
 use nrf52_phm as _; // global logger + panicking-behavior + memory layout
 
-use defmt::unwrap;
-use heapless::spsc::{Consumer, Producer, Queue};
-use phm_icd::{ToMcu, ToMcuI2c, ToPc, ToPcI2c};
-use postcard::{to_vec_cobs, CobsAccumulator, FeedResult};
-use usb_device::{
-    class_prelude::UsbBusAllocator,
-    device::{UsbDevice, UsbDeviceBuilder, UsbVidPid},
-};
-use usbd_serial::{SerialPort, USB_CLASS_CDC};
+#[rtic::app(device = nrf52840_hal::pac, dispatchers = [SWI0_EGU0])]
+mod app {
+    use cortex_m::singleton;
+    use defmt::unwrap;
+    use embedded_hal::blocking::i2c::Write;
+    use heapless::spsc::{Consumer, Producer, Queue};
+    use nrf52840_hal::{
+        clocks::{ExternalOscillator, Internal, LfOscStopped},
+        gpio::p1::Parts as P1Parts,
+        pac::{TIMER0, TWIM0},
+        twim::{Frequency, Pins as TwimPins, Twim},
+        usbd::{UsbPeripheral, Usbd},
+        Clocks,
+    };
+    use nrf52_phm::monotonic::{ExtU32, MonoTimer};
+    use phm_icd::{ToMcu, ToMcuI2c, ToPc, ToPcI2c};
+    use postcard::{to_vec_cobs, CobsAccumulator, FeedResult};
+    use usb_device::{
+        class_prelude::UsbBusAllocator,
+        device::{UsbDevice, UsbDeviceBuilder, UsbVidPid},
+    };
+    use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
-#[rtic::app(
-    device = nrf52840_hal::pac,
-    peripherals = true,
-    monotonic = groundhog_nrf52::GlobalRollingTimer,
+    #[monotonic(binds = TIMER0, default = true)]
+    type Monotonic = MonoTimer<TIMER0>;
 
-)]
-const APP: () = {
-    struct Resources {
+    #[shared]
+    struct Shared {}
+
+    #[local]
+    struct Local {
         inc_prod: Producer<'static, ToMcu, 8>,
         inc_cons: Consumer<'static, ToMcu, 8>,
         out_prod: Producer<'static, Result<ToPc, ()>, 8>,
         out_cons: Consumer<'static, Result<ToPc, ()>, 8>,
         usb_serial: SerialPort<'static, Usbd<UsbPeripheral<'static>>>,
-        cobs_buf: CobsAccumulator<512>,
         usb_dev: UsbDevice<'static, Usbd<UsbPeripheral<'static>>>,
         i2c: Twim<TWIM0>,
     }
 
-    #[init(schedule = [usb_tick])]
-    fn init(cx: init::Context) -> init::LateResources {
+    #[init(local = [
+        usb_bus: Option<UsbBusAllocator<Usbd<UsbPeripheral<'static>>>> = None,
+        incoming: Queue<ToMcu, 8> = Queue::new(),
+        outgoing: Queue<Result<ToPc, ()>, 8> = Queue::new(),
+    ])]
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
         let device = cx.device;
 
         // Ensure UICR NFC pins are disabled. This is to enable use of P0.09
@@ -55,8 +61,8 @@ const APP: () = {
         let clocks =
             unwrap!(singleton!(: Clocks<ExternalOscillator, Internal, LfOscStopped> = clocks));
 
-        // Configure the global timer, currently using TIMER0, a 32-bit, 1MHz timer
-        groundhog_nrf52::GlobalRollingTimer::init(device.TIMER0);
+        // Configure the monotonic timer, currently using TIMER0, a 32-bit, 1MHz timer
+        let mono = Monotonic::new(device.TIMER0);
 
         // // Create both GPIO ports for pin-mapping
         // let port0 = P0Parts::new(periphs.P0);
@@ -71,59 +77,46 @@ const APP: () = {
             Frequency::K100,
         );
 
-        // ------------------------------------------------------------
         // Set up USB Serial Port
-        //
-        // NOTE: Use of singleton!() is acceptable as we can only call
-        // this function safely once (as it consumes Peripherals).
-        // ------------------------------------------------------------
-        let (usb_dev, usb_serial) = {
-            let usb_bus = Usbd::new(UsbPeripheral::new(device.USBD, clocks));
-            let usb_bus = unwrap!(singleton!(:UsbBusAllocator<Usbd<UsbPeripheral>> = usb_bus));
+        let usb_bus = cx.local.usb_bus;
+        usb_bus.replace(Usbd::new(UsbPeripheral::new(device.USBD, clocks)));
+        let usb_serial = SerialPort::new(usb_bus.as_ref().unwrap());
+        let usb_dev = UsbDeviceBuilder::new(usb_bus.as_ref().unwrap(), UsbVidPid(0x16c0, 0x27dd))
+            .manufacturer("OVAR Labs")
+            .product("Powerbus Mini")
+            // TODO: Use some kind of unique ID. This will probably require another singleton,
+            // as the storage must be static. Probably heapless::String -> singleton!()
+            .serial_number("ajm123")
+            .device_class(USB_CLASS_CDC)
+            .max_packet_size_0(64) // (makes control transfers 8x faster)
+            .build();
 
-            let usb_serial = SerialPort::new(usb_bus);
-            let usb_dev = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x16c0, 0x27dd))
-                .manufacturer("OVAR Labs")
-                .product("Powerbus Mini")
-                // TODO: Use some kind of unique ID. This will probably require another singleton,
-                // as the storage must be static. Probably heapless::String -> singleton!()
-                .serial_number("ajm123")
-                .device_class(USB_CLASS_CDC)
-                .max_packet_size_0(64) // (makes control transfers 8x faster)
-                .build();
+        let (inc_prod, inc_cons) = cx.local.incoming.split();
+        let (out_prod, out_cons) = cx.local.outgoing.split();
 
-            (usb_dev, usb_serial)
-        };
-
-        let cobs_buf = CobsAccumulator::new();
-
-        let incoming = unwrap!(singleton!(: Queue<ToMcu, 8> = Queue::new()));
-        let outgoing = unwrap!(singleton!(: Queue<Result<ToPc, ()>, 8> = Queue::new()));
-
-        let (inc_prod, inc_cons) = incoming.split();
-        let (out_prod, out_cons) = outgoing.split();
-
-        cx.schedule.usb_tick(cx.start).ok();
-
-        init::LateResources {
-            inc_prod,
-            inc_cons,
-            out_prod,
-            out_cons,
-            usb_serial,
-            cobs_buf,
-            usb_dev,
-            i2c,
-        }
+        usb_tick::spawn().ok();
+        (
+            Shared {},
+            Local {
+                inc_prod,
+                inc_cons,
+                out_prod,
+                out_cons,
+                usb_serial,
+                usb_dev,
+                i2c,
+            },
+            init::Monotonics(mono),
+        )
     }
 
-    #[task(schedule = [usb_tick], resources = [usb_serial, cobs_buf, inc_prod, out_cons, usb_dev])]
+    #[task(local = [usb_serial, inc_prod, out_cons, usb_dev, cobs_buf: CobsAccumulator<512> = CobsAccumulator::new()])]
     fn usb_tick(cx: usb_tick::Context) {
-        let usb_serial = cx.resources.usb_serial;
-        let usb_dev = cx.resources.usb_dev;
-        let cobs_buf = cx.resources.cobs_buf;
-        let inc_prod = cx.resources.inc_prod;
-        let out_cons = cx.resources.out_cons;
+        let usb_serial = cx.local.usb_serial;
+        let usb_dev = cx.local.usb_dev;
+        let cobs_buf = cx.local.cobs_buf;
+        let inc_prod = cx.local.inc_prod;
+        let out_cons = cx.local.out_cons;
 
         let mut buf = [0u8; 128];
 
@@ -159,17 +152,16 @@ const APP: () = {
             Err(_e) => defmt::panic!("Usb Error!"),
         }
 
-        // Note: tick is in microseconds
-        cx.schedule.usb_tick(cx.scheduled + 1_000i32).ok();
+        usb_tick::spawn_after(1.millis()).ok();
     }
 
-    #[idle(resources = [inc_cons, out_prod, i2c])]
+    #[idle(local = [inc_cons, out_prod, i2c])]
     fn idle(cx: idle::Context) -> ! {
         defmt::println!("Hello, world!");
-        let i2c = cx.resources.i2c;
+        let i2c = cx.local.i2c;
 
         loop {
-            if let Some(data) = cx.resources.inc_cons.dequeue() {
+            if let Some(data) = cx.local.inc_cons.dequeue() {
                 match data {
                     ToMcu::I2c(ToMcuI2c::Write { addr, output }) => {
                         // embedded_hal::blocking::i2c::Write
@@ -178,22 +170,17 @@ const APP: () = {
                             Err(_) => Err(()),
                         };
 
-                        cx.resources.out_prod.enqueue(msg).ok();
+                        cx.local.out_prod.enqueue(msg).ok();
                     }
                     ToMcu::I2c(msg) => {
                         defmt::println!("unhandled I2C! {:?}", msg);
                     }
                     ToMcu::Ping => {
                         let msg: Result<_, ()> = Ok(ToPc::Pong);
-                        cx.resources.out_prod.enqueue(msg).ok();
+                        cx.local.out_prod.enqueue(msg).ok();
                     }
                 }
             }
         }
     }
-
-    // Sacrificial hardware interrupts
-    extern "C" {
-        fn SWI0_EGU0();
-    }
-};
+}
